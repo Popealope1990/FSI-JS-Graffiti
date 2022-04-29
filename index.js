5ac7208181dda410ec39f5108c74593b7991ba96
Benjamin Pope <buddypope1234@gmail.com>
2022-04-28 21:56:27 -0500

    made changes to index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for(let n = 0; n < dogName.length; n++){
+	dogName[n].style.textAlign = "left"
+}
